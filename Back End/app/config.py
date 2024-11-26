f0cc4561672b324c6d5a7dde6034b7cfefc552c2
Arian Fooladray <AFooladray@fgfbrands.com>
2024-11-26 16:02:46 -0500

    d

---
+++ Back End/app/config.py
@@ -0,0 +1,8 @@
+# Azure DevOps Configuration
+ORG_NAME = 'preet442727'  # Replace with your Azure DevOps organization name
+PROJECT_NAME = 'test'  # Replace with your Azure DevOps project name
+PAT = 'VJ2qQQgHniwwpMroZeGtayYSqnmVympa62s7oBOtc9DW0rJCjKo0JQQJ99AKACAAAAAAAAAAAAASAZDOE742'  # Replace with your Azure DevOps Personal Access Token
+
+# Base URLs
+AZURE_DEVOPS_GRAPH_API_URL = f"https://vssps.dev.azure.com/{ORG_NAME}/_apis/graph"
+AZURE_DEVOPS_REST_API_URL = f"https://dev.azure.com/{ORG_NAME}/{PROJECT_NAME}/_apis"
