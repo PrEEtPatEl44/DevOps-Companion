16d45d82cab596f90709297d9467714871261794
Preet Patel <pbpatel48@myseneca.ca>
2024-12-03 23:15:16 -0500

    upadated

--- next-shadcn-dashboard-starter/app/dashboard/overview/page.tsx
+++ next-shadcn-dashboard-starter/app/dashboard/overview/page.tsx
@@ -1,9 +1,31 @@
+"use client"
 import OverViewPage from './_components/overview';
 
-export const metadata = {
-  title: 'Dashboard : Overview'
-};
+import { useState, useEffect } from 'react';
+// export const metadata = {
+//   title: 'Dashboard : Overview'
+// };
 
-export default function page() {
+
+export default function Page() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); 
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return ( <div className="loading-overlay">
+      <div className="loading-content">
+        <img src="/Loader.gif" alt="Loading..." />
+      </div>
+    </div>)
+  }
+
   return <OverViewPage />;
 }
+
